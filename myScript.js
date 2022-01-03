51922ee52a4a4a5815703a6a902a4134f9305bed
SF-codes <felicissimo.shanis@gmail.com>
2022-01-03 13:50:50 -0500

    Added Scrolltotop button and refactored code!

--- myScript.js
+++ myScript.js
@@ -13,3 +13,28 @@ function togglePopup2() {
 function togglePopup3() {
   document.getElementById("popup-4").classList.toggle("active");
 }
+
+const btnScrollToTop = document.querySelector("#scroll-to-top-btn");
+
+const handleScroll = function () {
+  const ratio = 0.5;
+  const scrollTop = document.documentElement.scrollTop;
+  const scrolledVSClientHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+
+  if (scrollTop / scrolledVSClientHeight > ratio) {
+    btnScrollToTop.style.display = "block";
+  } else {
+    btnScrollToTop.style.display = "none";
+  }
+};
+document.addEventListener("scroll", handleScroll);
+
+btnScrollToTop.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
